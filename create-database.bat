5dbbb9ef71e3bae68e70565ace585c4bb5ef7202
Anantha-Gowtham <ananthagowthamgunde@gmail.com>
2025-09-25 11:48:40 +0530

    Remove unnecessary files: IDE configs, Maven distribution, logs, scripts, docs, and migrations

--- create-database.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Creating OBS Banking System Database...
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -proot -e "CREATE DATABASE IF NOT EXISTS obs_banking_system;"
-echo Database created successfully!
-pause
